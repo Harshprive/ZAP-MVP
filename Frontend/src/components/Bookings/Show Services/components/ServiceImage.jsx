a3584152ee893e67efc7aa0e56baf055732c6de5
Sahil Jaronde <jarondesahil18@gmail.com>
2025-05-09 10:40:12 +0530

    frontend folder deleted

--- Frontend/src/components/Bookings/Show Services/components/ServiceImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ServiceImage = ({ src, alt }) => (
-  <div className="w-full h-90 overflow-hidden rounded-lg  ">
-    <img src={src} alt={alt} className="w-full h-full object-fit" />
-  </div>
-);
-
-export default ServiceImage;
